6d65f6d62a384b227142a20151bf7db1e0b82f5b
ilya-melnik <ilay.melnik.kr@gmail.com>
2020-11-18 10:08:07 +0200

    theAnd17.1

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,7 +5,6 @@ import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
-
         <nav className={s.nav}>
             <div className={`${s.item} ${s.active}`}>
                 <NavLink to="/profile" activeClassName={s.active}>Profile</NavLink>
