03285cfcfd526d851386ef7109098181862a8b1d
ilya-melnik <ilay.melnik.kr@gmail.com>
2021-05-18 23:12:39 +0300

    import, export, first_component

---
+++ src/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Header = () => {
+    return (
+        <div>
+            <a href={'#H'}>Home</a>
+            <a href={'#H'}>News Feed</a>
+            <a href={'#H'}>Message</a>
+        </div>
+    )
+}
+export default Header;
