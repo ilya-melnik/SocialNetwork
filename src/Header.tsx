00a882d400a3f05ee6a50ca0c6b75afc31be1442
ilya-melnik <ilay.melnik.kr@gmail.com>
2021-05-19 23:46:32 +0300

    start grid

--- src/Header.tsx
+++ src/Header.tsx
@@ -4,9 +4,9 @@ import React from "react";
 const Header = () => {
     return (
         <div>
-            <a href={'#H'}>Home</a>
-            <a href={'#H'}>News Feed</a>
-            <a href={'#H'}>Message</a>
+            <a href={'#H'}>Home</a>-
+            <a href={'#H'}>News Feed</a>-
+            <a href={'#H'}>Message</a>-
         </div>
     )
 }
